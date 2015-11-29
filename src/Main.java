9003462c33263fb24825e8449d7b29bcbce80fb8
Jorge <jogonzal@microsoft.com>
2015-11-28 18:07:28 -0800

    Add bigdata folder - now running code!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import edu.berkeley.nlp.assignments.decoding.MtDecoderTester;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        MtDecoderTester.main(new String[]{
+                "-path" ,".\\bigdata",
+                "-decoderType", "MONO_GREEDY"
+        });
     }
 }
